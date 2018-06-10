c16179e2176cd3b3c988b60e7afc442bb26465d3
Felipe Oliveira <Fel_oliveira_silva@yahoo.com.br>
2018-06-10 20:04:12 -0300

    Algoritmos de ordenação

---
+++ src/AED/QuickSort.java
@@ -0,0 +1,30 @@
+package AED;
+
+public class QuickSort {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int[] vetor = { 20, 40, 30, 60, 50 };
+		insertionSort(vetor);
+	}
+
+	
+	
+	/**
+	 * Algoritmo de ordena��o por inser��o se baseia em ordenar da esquerda para
+	 * a direita
+	 * 
+	 * @param vetor
+	 */
+	private static void insertionSort(int[] vetor) {
+		int i;
+		int j;
+		int k;
+
+		for (j = 1; j < vetor.length; j++) {
+
+		}
+	}
+}
